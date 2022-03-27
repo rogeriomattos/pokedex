319b2ff61f623b0357b41ebce3b74efa5b65afee
Rogerio Almeida <rogerio.mattos@stoom.com.br>
2022-03-27 15:00:43 -0300

    feat: creating footer component

---
+++ src/components/molecules/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <footer>
+    <a
+      href="https://github.com/rogeriomattos"
+      target="_blank"
+    >
+      Developed by Rogerio Almeida de Mattos
+    </a>
+  </footer>
+)
+
+export default Footer;
